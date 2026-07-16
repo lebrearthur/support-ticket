9989b97d4cd6a44c1eb726b5b4fe48e77359669d
Arthur Krause Lebre <lebrearthur@hotmail.com>
2026-07-16 10:20:02 -0400

    Adding Update, Updating Status and Delete functions

---
+++ src/controllers/tickets/updateStatus.js
@@ -0,0 +1,8 @@
+export function updateStatus({ request, response, database }) {
+    const { id } = request.params
+    const { solution } = request.body
+    
+    console.log(solution)
+    database.update("tickets", id, { status: "closed", solution})
+    return response.writeHead(200).end()
+}
